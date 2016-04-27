da8f9feec26f90eb418856599ddf12b0143aeb8f
Lucas Treffenstädt <lucas@treffenstaedt.de>
2016-04-27 14:16:42 +0200

    New tools

--- tools/run_hysteresis.sh
+++ tools/run_hysteresis.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-root=$(pwd)"/"
-for conffile in "$@"
-do
-	sim_rundir="/home/lucas/Simulation/run/hysteresis/$conffile/"
-	mkdir -p $sim_rundir
-	echo $sim_rundir
-	cd /home/lucas/Simulation/bin_squarewell/build
-	cp $root$conffile ../config.h
-	make clean
-	make -j
-	cp ../bin/explosive $sim_rundir
-	cd $sim_rundir
-	./explosive 1> log 2> error.log
-done
+if [ $# -ne 3 ]
+then
+	echo "Usage: run_hysteresis <dirname> <template> <squarewell/continuous>"
+else
+	root=$(pwd)"/"
+	for conffile in $(ls $2*)
+	do
+		sim_rundir="/home/lucas/Simulation/run/$1/$conffile/"
+		mkdir -p $sim_rundir
+		echo $sim_rundir
+		cd /home/lucas/Simulation/bin_$3/build
+		cp $root$conffile ../config.h
+		make clean
+		make -j
+		cp ../bin/explosive $sim_rundir
+		cd $sim_rundir
+		./explosive 1> log 2> error.log
+	done
+fi
